f701c8a7033ed3c1ad041e249f9734c379f52c92
beresdev <beresdev@gmail.com>
2023-01-20 17:01:39 -0600

    moviendo archivo de config y agregandolo a gitignore

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 // Este es el punto de entrada de tu aplicacion
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js';
+import firebaseConfig from './ConfigFirebase.js';
 import { register } from './views/register.js';
 import { getAuth } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js';
 import { emailAutentication} from './Firebase/FirebaseFunctions.js';
-import firebaseConfig from './Firebase/ConfigFirebase.js';
 
-initializeApp(firebaseConfig)
+initializeApp(firebaseConfig);
 register();
 
 const submit = document.getElementById('enviar')
