d747fb48157370c4df6b7d97e41a8002cc4d8646
jeannie alvarado <jeaniie38@gmail.com>
2023-01-20 16:12:42 -0600

    autencion de usuario con email

--- src/main.js
+++ src/main.js
@@ -1,31 +1,24 @@
 // Este es el punto de entrada de tu aplicacion
-
-import { register } from './views/register.js';
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js';
-import { getAuth, createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js';
-import firebaseConfig from './Firebase/ConfigFirebase.js'
-
+import { register } from './views/register.js';
+import { getAuth } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js';
+import { emailAutentication} from './Firebase/FirebaseFunctions.js';
+import firebaseConfig from './Firebase/ConfigFirebase.js';
 
 initializeApp(firebaseConfig)
 register();
 
 const submit = document.getElementById('enviar')
+
 const auth = getAuth();
 
 submit.addEventListener('click', (e) => {
-   // e.preventDefault();
+    e.preventDefault();
     let email = document.getElementById('correo').value
     //let usuaria = document.getElementById('usuaria').value
     let password = document.getElementById('password').value
     console.log(email)
     console.log(password)
-    createUserWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-
-            const user = userCredential.user;
-        })
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-        });
+    //registerFirebase(auth, email, password)
+    emailAutentication(auth, email)
 })
