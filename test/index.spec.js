f3e7cc038d0d1af01bdc3f570e43fdf289c6a740
alerileri <ale.ortmed@gmail.com>
2023-01-27 17:47:59 -0600

    Intento1Testing

--- test/index.spec.js
+++ test/index.spec.js
@@ -8,8 +8,22 @@ import { registerFirebase } from '../src/Firebase/FirebaseFunctions';
 // });
 
 
+
+// describe('registerFirebase', () => {
+//   it('debería ser una función', () => {
+//     expect(typeof myFunction).toBe('function');
+//   });
+// });
+
+
 describe('registerFirebase', () => {
-  it('debería ser una función', () => {
-    expect(typeof myFunction).toBe('function');
-  });
-});
+  let email = 'correo@gmail.com';
+  //let password = 'notarealpassword';
+  let passwordVacio = '';
+  //let emailVacio = '';
+
+  it('debe mostrar un mensaje de error cuando no se entregue un mail como argumento', () => {
+    expect(registerFirebase(auth, email, passwordVacio).toThrowError(TypeError))
+  })
+
+})
