81751fb8ac43aea07bfc27534961bc56637d2f04
alerileri <ale.ortmed@gmail.com>
2023-01-30 17:23:10 -0600

    resolución de errores

--- src/lib/Router.js
+++ src/lib/Router.js
@@ -1,40 +1,38 @@
+const routes = {};
+const templates = {};
 
-let routes = {};
-let templates = {};
+export const route = (path, template) => {
+  // Le entregamos dos parámetros a la función route: path('#/' o '#/About')
+  // y template (las funciones home y about)
 
-
-export const route = (path, template) => { //Le entregamos dos parámetros a la función route: path('#/' o '#/About') 
-    //y template (las funciones home y about)
-
-    if (typeof template === 'function') {
-        //Validamos si el typeof template es una función
-        return routes[path] = template; // El contenido de las funciones se reescribe?
-
-    } else if (typeof template === 'string') {
-        //Validamos si el typeof template es un string (nombre de archivo)
-        return routes[path] = templates[template]; // La función crea elementos del DOM
-
-    } else {
-        return; // Si no, no retorna nada, se provoca un error
-    }
+  if (typeof template === 'function') {
+    // Validamos si el typeof template es una función
+    return routes[path] = template; // El contenido de las funciones se reescribe?
+  } if (typeof template === 'string') {
+    // Validamos si el typeof template es un string (nombre de archivo)
+    return routes[path] = templates[template]; // La función crea elementos del DOM
+  }
+  // Si no, no retorna nada, se provoca un error
 };
 
-export const template = (name, templateFunction) => { //La función se llama template y espera dos parámetros
-    return templates[name] = templateFunction; // El objeto template recibe las funciones home() o about()
-};
+// La función se llama template y espera dos parámetros
+export const template = (name, templateFunction) => 
+  templates[name] = templateFunction;
+  // El objeto template recibe las funciones home() o about()
 
 export const resolveRoute = (route) => {
-    try {
-        return routes[route]; // Retorna el objeto ruta con el parámetro route
-    }
-    catch (e) {
-        throw new Error(`Route ${route} not found`); //En caso de que la ruta no exista, arroja error
-    };
+  try {
+    return routes[route]; // Retorna el objeto ruta con el parámetro route
+  } catch (e) {
+    throw new Error(`Route ${route} not found`); // En caso de que la ruta no exista, arroja error
+  }
 };
 
 export const router = () => {
-    let url = window.location.hash.slice(1) || '/'; // guarda el valor de la ruta después del hash (gato o michi)
-    console.log(url)
-    let route = resolveRoute(url); // Declara la variable route y le asigna lo que regresa la función resolveRoute (la url)
-    route() //Manda llamar a la función route
+  const url = window.location.hash.slice(1) || '/';
+  // guarda el valor de la ruta después del hash (gato o michi)
+  console.log(url);
+  const route = resolveRoute(url);
+  // Declara la variable route y le asigna lo que regresa la función resolveRoute (la url)
+  route(); // Manda llamar a la función route
 };
--- src/views/register.js
+++ src/views/register.js
@@ -1,71 +1,70 @@
-let main = document.getElementById('main');
-let footer = document.getElementById('footer');
+const main = document.getElementById('main');
+const footer = document.getElementById('footer');
 
 export const register = () => {
-    main.innerHTML = '';
-    footer.innerHTML = '';
-    let sectionA = document.createElement('section');
-    let logo = document.createElement('img');
-    let sectionB = document.createElement('section');
-    let registrate = document.createElement('h2');
-    let sectionC = document.createElement('section');
-    let form = document.createElement('form');
-    let correo = document.createElement('label');
-    let inputCorreo = document.createElement('input');
-    let usuaria = document.createElement('label');
-    let inputUsuaria = document.createElement('input');
-    let password = document.createElement('label');
-    let inputPassword = document.createElement('input');
-    let button = document.createElement('button');
-    const p = document.createElement('p');
+  main.innerHTML = '';
+  footer.innerHTML = '';
+  const sectionA = document.createElement('section');
+  const logo = document.createElement('img');
+  const sectionB = document.createElement('section');
+  const registrate = document.createElement('h2');
+  const sectionC = document.createElement('section');
+  const form = document.createElement('form');
+  const correo = document.createElement('label');
+  const inputCorreo = document.createElement('input');
+  const usuaria = document.createElement('label');
+  const inputUsuaria = document.createElement('input');
+  const password = document.createElement('label');
+  const inputPassword = document.createElement('input');
+  const button = document.createElement('button');
+  const p = document.createElement('p');
 
-    sectionA.className = 'section-logo';
-    sectionA.appendChild(logo);
-    logo.src = './img/logo-big.png';
-    logo.alt = 'logo-powerL';
-    sectionB.className = 'section-h2';
-    sectionB.appendChild(registrate);
-    registrate.innerText = 'Regístrate';
+  sectionA.className = 'section-logo';
+  sectionA.appendChild(logo);
+  logo.src = './img/logo-big.png';
+  logo.alt = 'logo-powerL';
+  sectionB.className = 'section-h2';
+  sectionB.appendChild(registrate);
+  registrate.innerText = 'Regístrate';
 
-    sectionC.className = 'section-form';
+  sectionC.className = 'section-form';
 
-    sectionC.appendChild(form);
-    form.appendChild(correo);
-    form.appendChild(inputCorreo);
-    inputCorreo.type = 'email';
-    inputCorreo.placeholder = 'correo@dominio.com';
-    inputCorreo.id = 'correo';
-    inputCorreo.required='required';
+  sectionC.appendChild(form);
+  form.appendChild(correo);
+  form.appendChild(inputCorreo);
+  inputCorreo.type = 'email';
+  inputCorreo.placeholder = 'correo@dominio.com';
+  inputCorreo.id = 'correo';
+  inputCorreo.required = 'required';
 
-    correo.innerText = 'Correo';
-    usuaria.innerText = 'Usuaria';
-    password.innerText = 'Contraseña';
+  correo.innerText = 'Correo';
+  usuaria.innerText = 'Usuaria';
+  password.innerText = 'Contraseña';
 
-    form.appendChild(usuaria);
-    form.appendChild(inputUsuaria);
-    inputUsuaria.type = 'text';
-    inputUsuaria.placeholder = 'usuariaCoolDEV002';
-    inputUsuaria.id='usuaria'
+  form.appendChild(usuaria);
+  form.appendChild(inputUsuaria);
+  inputUsuaria.type = 'text';
+  inputUsuaria.placeholder = 'usuariaCoolDEV002';
+  inputUsuaria.id = 'usuaria';
 
-    form.appendChild(password);
-    form.appendChild(inputPassword);
-    inputPassword.type = 'password';
-    inputPassword.placeholder = '******';
-    inputPassword.id= 'password'
-    inputPassword.minLength= '6'
-    inputPassword.maxLength='12'
-    inputPassword.required='required'
+  form.appendChild(password);
+  form.appendChild(inputPassword);
+  inputPassword.type = 'password';
+  inputPassword.placeholder = '******';
+  inputPassword.id = 'password';
+  inputPassword.minLength = '6';
+  inputPassword.maxLength = '12';
+  inputPassword.required = 'required';
 
-    form.appendChild(button);
-    button.innerText = 'Enviar';
-    button.type = 'submit';
-    button.id= 'enviar'
-    main.appendChild(sectionA);
-    main.appendChild(sectionB);
-    main.appendChild(sectionC);
+  form.appendChild(button);
+  button.innerText = 'Enviar';
+  button.type = 'submit';
+  button.id = 'enviar';
+  main.appendChild(sectionA);
+  main.appendChild(sectionB);
+  main.appendChild(sectionC);
 
-    p.innerText = 'Desarrollado por y para Laboratorians';
+  p.innerText = 'Desarrollado por y para Laboratorians';
 
-    footer.appendChild(p);
-}
-
+  footer.appendChild(p);
+};
