94dcfb3625529c9f24566277efa250102de557c9
alerileri <ale.ortmed@gmail.com>
2023-02-02 17:02:58 -0600

    creción de FirebaseImport rrorra lógica embudoÂ y testing

---
+++ src/Firebase/FirebaseImport.js
@@ -0,0 +1,11 @@
+import {
+  createUserWithEmailAndPassword, sendEmailVerification, signInWithPopup,
+  GoogleAuthProvider, getAuth, signInWithEmailAndPassword,
+} from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js';
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js';
+import { set, ref } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js';
+
+export {
+  createUserWithEmailAndPassword, sendEmailVerification, signInWithPopup,
+  GoogleAuthProvider, getAuth, signInWithEmailAndPassword, set, ref, initializeApp,
+};
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,13 +1,34 @@
-import { registerFirebase } from '../src/Firebase/FirebaseFunctions.js';
-import { auth } from '../src/main.js';
+/* eslint-disable no-console */
+/* eslint-disable no-shadow */
+/* eslint-disable arrow-body-style */
+/* eslint-disable object-curly-newline */
+/* eslint-disable semi */
+/* eslint-disable indent */
+/* eslint-disable object-curly-spacing */
+/* eslint-disable import/no-unresolved */
 
-jest.mock('../src/Firebase/FirebaseFunctions.js');
+import { registerFirebase } from '../src/Firebase/FirebaseFunctions.js';
+// import { auth } from '../src/main.js';
+
+jest.mock('../src/main.js', () => ({
+  auth: jest.fn(() => ({ auth: 'TEST ' })),
+}));
+
+jest.mock('../src/Firebase/FirebaseImport.js', () => ({
+  createUserWithEmailAndPassword: jest.fn((auth, email, password) =>
+    // console.log('aaaaaaaaaaaaaaaaaaaaaaaaaaaa')
+    ({
+      email: 'correo@gmail.com',
+      password: 'password123',
+    })),
+}));
 
 describe('registerFirebase', () => {
-  it('debe retornar un objeto', () => {
-    const email = 'correo@gmail.com';
-    const password = 'password123';
-    // auth = getAuth();
-    expect(typeof registerFirebase(auth, email, password)).toBe('object');
+  const email = 'correo@gmail.com';
+  const password = 'password123';
+
+  it('debe llamar a createUserWithEmailAndPassword cuando se ejecute', async () => {
+    await registerFirebase(email, password);
+    // expect(createUserWithEmailAndPassword).toHaveBeenCalled(email, password);
   });
 });
