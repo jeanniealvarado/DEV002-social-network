67385ed61d23bb900c5f53aec5070bb6a46ce35f
jeannie alvarado <jeaniie38@gmail.com>
2023-02-02 16:40:13 -0600

    aceptar cambios de tests

--- test/index.spec.js
+++ test/index.spec.js
@@ -3,25 +3,6 @@ import { auth } from '../src/main.js';
 
 jest.mock('../src/Firebase/FirebaseFunctions.js');
 
-<<<<<<< HEAD
-// describe('registerFirebase', () => {
-//   it('debería ser una función', () => {
-//     expect(typeof myFunction).toBe('function');
-//   });
-// });
-
-// describe('registerFirebase', () => {
-//   let email = 'correo@gmail.com';
-//   //let password = 'notarealpassword';
-//   let passwordVacio = '';
-//   //let emailVacio = '';
-
-//   it('debe mostrar un mensaje de error cuando no se entregue un mail como argumento', () => {
-//     expect(registerFirebase(auth, email, passwordVacio).toThrowError(TypeError))
-//   })
-
-// })
-=======
 describe('registerFirebase', () => {
   it('debe retornar un objeto', () => {
     const email = 'correo@gmail.com';
@@ -30,4 +11,3 @@ describe('registerFirebase', () => {
     expect(typeof registerFirebase(auth, email, password)).toBe('object');
   });
 });
->>>>>>> df21a18482bf23e1d1c3cf3b8d075a8f2a46baa8
