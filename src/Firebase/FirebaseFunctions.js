e307028cbe55a8eb5c0ba3b8579d1c3096bc06e3
alerileri <ale.ortmed@gmail.com>
2023-02-14 19:18:11 -0600

    Commit para traer cambios de dev

--- src/Firebase/FirebaseFunctions.js
+++ src/Firebase/FirebaseFunctions.js
@@ -95,7 +95,7 @@ export const updateNotes = (id, newFile) => updateDoc(doc(db, 'users', id), newF
 export const getPost = (id) => getDoc(doc(db, 'users', id));
 export const editLike = (id) => getDoc(doc(db, 'users', id));
 export const datePost = (querySnapshot) => {
-  const q = query(collection(db, 'tasks'), orderBy('createdDateTime', 'desc'));
+  const q = query(collection(db, 'users'), orderBy('createdDateTime', 'desc'));
   onSnapshot(q, querySnapshot);
 };
 
